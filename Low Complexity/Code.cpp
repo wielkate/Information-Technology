64b1a134f0f7ffb1ad065e1a5307cc1cb366d7fa
Ekaterina Mashchenko <80549277+wielkate@users.noreply.github.com>
2022-10-28 01:33:00 +0200

    Update Code.cpp
    Program wywołuje się z linii poleceń, zmieniono styl wypisywania sekwencji.

--- Low Complexity/Code.cpp
+++ Low Complexity/Code.cpp
@@ -16,46 +16,34 @@
 #include <unordered_set>
 #include <fstream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
-struct STRUCTURE {
-  int wide; // szerokosc okna
-  float threshold; // prog, od ktorego uznajemy lc
-};
+/* wprowadzenie argumentow z koncoli - dazymy do formatu file.cpp seq.fasta -w 10 -t 0.9 > out.txt
+ * Have 6 arguments:
+ * i = 0 => file.cpp
+ * i = 1 => seq.fasta
+ * i = 2 => -w
+ * i = 3 => 10
+ * i = 4 => -t
+ * i = 5 => 0.9
+ * Z powyzszego wydruku widzimy, ze sa potezebne argumenty o indeksach 1 (nazwa pliku do odczytu),
+ * 3 jako szerokosc okna oraz 5 jako prog od ktorego uznajemy zlozonosc za niska */
 
-STRUCTURE parameters;
-
-//================================================================================= funkcja sprawdza poprawnosc danych
-void Check(int length) {
-
-  while (!(parameters.wide <= length && parameters.wide >= 1)) {
-	cout << "Szerokosc nie moze byc ani ujemna, ani wieksza od dlugosci sekwencji = " << length << endl;
-	cin >> parameters.wide;
-  }
-  while (!(parameters.threshold <= 1 && parameters.threshold >= 0)) {
-	cout << "T miesci sie w przedziale od 0 do 1" << endl;
-	cin >> parameters.threshold;
-  }
-}
-
-int main() {
+int main(int argc, char **argv) {
 
   //================================================================================= definicja zmiennych
-  fstream file_from, file_to; // pliki dla odczytu i zapisu
-  string file_name; // nazwa pliku
-  string sequence; // sekwencja
+  fstream file_from; // plik dla odczytu
+  string sequence; // sekwencja oryginalna
   string cut_sequence; // sekwencja po wprowadzeniu szerokosci okna
-  unordered_set<string> result; // set dla wynikowych lancuchow
-
+  unordered_set<string> result; // set z uwzglednieniem progu zlozonosci
   unsigned int variance; // wariancja z powtorzeniami
   float lc; // zlozonosc lingwistyczna
 
   //================================================================================= odczyt pliku
   while (true) {
-	cout << "Prosze podac istniejaca nazwe pliku z rozszerzeniem: " << endl;
-	cin >> file_name;
-	file_from.open(file_name.c_str(), fstream::in | fstream::_Nocreate );
+	file_from.open(string(argv[1]).c_str(), fstream::in);
 
 	if (file_from.good()) {
 	  getline(file_from, sequence); // fasta jest zapisany w jednej linii
@@ -63,22 +51,27 @@ int main() {
 	  file_from.close();
 	  break;
 	}
-	file_from.close();
+	  cout << "Plik nie istnieje, prosze sprobowac ponownie" << endl;
+	  cin >> argv[1];
+	  file_from.close();
   }
 
-  //================================================================================= wprowadzenie parametrow
-  cout << "Wprowadzic parametry W oraz T" << endl;
-  cin >> parameters.wide >> parameters.threshold;
-  Check(sequence.length());
+  //================================================================================= sprawdzenie poprawnosci danych
+	while (!(stoi(argv[3]) <= sequence.length() && stoi(argv[3]) >= 1)) {
+	  cout << "Szerokosc nie moze byc mniejsza od jedynki ani wieksza od dlugosci sekwencji = " << sequence.length() << endl;
+	  cin >> argv[3];
+	}
+	while (!(stof(argv[5]) <= 1 && stof(argv[5]) >= 0)) {
+	  cout << "T miesci sie w przedziale od 0 do 1" << endl;
+	  cin >> argv[5];
+	}
 
   //================================================================================= algorytm
-
-  for (int window_start = 0; window_start + parameters.wide <= sequence.length();
-	   window_start++) { // wprowadzenie przesuwania
-	cut_sequence = sequence.substr(window_start, parameters.wide); // ograniczenie sekwencji do rozmiaru okna
+  for (int window_start = 0; window_start + stoi(argv[3]) <= sequence.length(); window_start++) { // wprowadzenie przesuwania
+	cut_sequence = sequence.substr(window_start, stoi(argv[3])); // ograniczenie sekwencji do rozmiaru okna
 
 	// inicjalizacja zostala przeniesiona tu dla wyzerowania zmiennych
-	unordered_set<string> substring; // set dla podciagow
+	unordered_set<string> substring; // set z uwzglednieniem unikatowosci podlancuchow
 	unsigned int distance = 1; // liczba nukleotydow, zaczynajac od mononukleotydu
 	unsigned int max_substring = 0; // mianownik (liczba podciagow alfabetu)
 	unsigned int counter = 0; // licznik slow w jednym cyklu
@@ -98,18 +91,17 @@ int main() {
 	  }
 	}
 
-	// jesli region ma niska zlozonosc, to dodaje sie do tablicy wynikowej
+	// jesli region ma niska zlozonosc, to dodaje zbior do tablicy wynikowej z jednoczesnym wypisywaniem
 	lc = float(substring.size()) / float(max_substring); // rzutowanie typu
-	if (lc <= parameters.threshold && !result.count(cut_sequence)) result.insert(cut_sequence);
+	if (lc <= stof((argv[5])) && !result.count(cut_sequence)) {
+	  result.insert(cut_sequence);
+	  // Przykladowy wyglad : widoczna jest nazwa pliku bez rozszerzenia, polozenie liczy sie od 1
+	  // >id seq 50:58
+	  // AAAA ...
+	  cout << ">id " << string(argv[1]).substr(0,string(argv[1]).find_last_of('.')) << " " << window_start + 1 << ":" << window_start + stoi(argv[3]) << endl;
+	  cout << cut_sequence << endl;
+	}
   } // koniec algorytmu
-
-  //================================================================================= zapis pliku
-  cout << "Prosze wpisac nazwe pliku wynikowego " << endl;
-  cin >> file_name;
-  // otwieranie nieistniejacego pliku spowoduje jego utworzenie
-  file_to.open(file_name.c_str(), fstream::out | fstream::app);
-  for (const auto &item : result) file_to << item << endl;
-  file_to.close();
 
   return 0;
 }
